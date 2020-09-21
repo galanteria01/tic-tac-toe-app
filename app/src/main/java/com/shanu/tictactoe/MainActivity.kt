3be9c9d4ad665b01ff96a95334e5961801dafe26
galanteria01 <shanuu12e@gmail.com>
2020-09-21 07:59:03 +0530

    Made few more corrections

--- app/src/main/java/com/shanu/tictactoe/MainActivity.kt
+++ app/src/main/java/com/shanu/tictactoe/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,7 +17,7 @@ class MainActivity : AppCompatActivity() {
         var selectedButton = view as Button
         Log.d("buclick:", selectedButton.id.toString())
         var cellId = 0
-        when(selectedButton.id){
+        when (selectedButton.id) {
             R.id.button1 -> cellId = 1
             R.id.button2 -> cellId = 2
             R.id.button3 -> cellId = 3
@@ -28,27 +29,96 @@ class MainActivity : AppCompatActivity() {
             R.id.button9 -> cellId = 9
 
         }
-
-
+        playGame(cellId,selectedButton)
 
 
     }
+
     var activePlayer = 1
     var playerOne = ArrayList<Int>()
     var playerTwo = ArrayList<Int>()
-    fun playGame(cellId:Int, selectedbutton:Button){
+    fun playGame(cellId: Int, selectedbutton: Button) {
 
-        if(activePlayer == 1){
+        if (activePlayer == 1) {
             selectedbutton.text = "X"
             selectedbutton.setBackgroundResource(R.color.cyan)
+            selectedbutton.setTextColor(R.color.black)
             playerOne.add(cellId)
+            activePlayer = 2
 
 
-
-        }else{
+        } else {
             selectedbutton.text = "O"
             selectedbutton.setBackgroundResource(R.color.white)
+            selectedbutton.setTextColor(R.color.black)
             playerTwo.add(cellId)
+            activePlayer = 1
+
+        }
+        checkWinner()
+
+    }
+
+    fun checkWinner() {
+        var winner = -1
+
+        // row 1
+        if (playerOne.contains(1) && playerOne.contains(2) && playerOne.contains(3)) {
+            winner = 1
+        }
+        else if (playerTwo.contains(1) && playerTwo.contains(2) && playerTwo.contains(3)) {
+            winner = 2
+        }
+
+        //row 2
+        if (playerOne.contains(4) && playerOne.contains(5) && playerOne.contains(6)) {
+            winner = 1
+        }
+        else if (playerTwo.contains(4) && playerTwo.contains(5) && playerTwo.contains(6)) {
+            winner = 2
+        }
+
+        //row 3
+        if (playerOne.contains(7) && playerOne.contains(8) && playerOne.contains(9)) {
+            winner = 1
+        }
+        else if (playerTwo.contains(7) && playerTwo.contains(8) && playerTwo.contains(9)) {
+            winner = 2
+        }
+
+        // col 1
+        if (playerOne.contains(1) && playerOne.contains(4) && playerOne.contains(7)) {
+            winner = 1
+        }
+        else if (playerTwo.contains(1) && playerTwo.contains(4) && playerTwo.contains(7)) {
+            winner = 2
+        }
+
+        //col 2
+        if (playerOne.contains(2) && playerOne.contains(5) && playerOne.contains(8)) {
+            winner = 1
+        }
+        else if (playerTwo.contains(2) && playerTwo.contains(5) && playerTwo.contains(8)) {
+            winner = 2
+        }
+
+        //col 3
+        if (playerOne.contains(3) && playerOne.contains(6) && playerOne.contains(9)) {
+            winner = 1
+        }
+        else if (playerTwo.contains(3) && playerTwo.contains(6) && playerTwo.contains(9)) {
+            winner = 2
+        }
+
+        // Final check
+        if(winner == 1){
+            Toast.makeText(this,"Player one wins ",Toast.LENGTH_LONG).show()
+
+        }else if(winner ==2){
+            Toast.makeText(this,"Player two wins ",Toast.LENGTH_LONG).show()
+
+        }else{
+            Toast.makeText(this,"Game is tie ",Toast.LENGTH_LONG).show()
 
         }
 
